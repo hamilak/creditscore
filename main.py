cbbc7db87625faf5a5db74fadd5ccdadcaf7c0fa
hamilaK2 <aa.kalimah00@gmail.com>
2022-11-15 16:33:21 +0100

    first commit

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import pandas as pd
+import sklearn
 import numpy as np
 
 # loading csv file
 df = pd.read_csv('credit.csv')
-# print(df.head(5))
 
+# dropping columns
 cols = ['credit_history', 'purpose', 'personal_status', 'property', 'other_debtors', 'age', 'installment_plan', 'housing',
      'dependents', 'telephone', 'foreign_worker']
 new_df = df.drop(cols, axis='columns')
-
 # print(new_df.head(5))
-# print(new_df.info())
-print(new_df.isnull)
+print(new_df.info())
+# print(new_df.isnull)
 print(new_df.isna().any().sum())
+# # drop rows with missing values
+# new_df = new_df.dropna()
+# print(new_df.isnull)
+# print(new_df.isna().any().sum())
 
-new_df = new_df.dropna()
-print(new_df.isnull)
-print(new_df.isna().any().sum())
-
-# ratio of male to female workers
-print(new_df['gender'].value_counts())
-
+Y = new_df['default']
